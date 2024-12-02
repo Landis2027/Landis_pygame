c2602b9180bcc247fb61019cf9f155b59359f4f9
Landis2027 <m273582@usna.edu>
2024-12-02 08:17:04 -0500

    Title screen

--- game.py
+++ game.py
@@ -12,9 +12,19 @@ pygame.mixer.init()
 bg_music = pygame.mixer.Sound('assets/WiiPlayTanksMusic.mp3')
 bg_music.play(-1)
 
+#Color Palette
+night = pygame.Color('#0F0F0F')
+rose_quartz = pygame.Color('#e9bec6')
+jet = pygame.Color('#2D2E2E')
+dim_gray = pygame.Color('#716969')
+white = pygame.Color('#FBFBFB')
+burgundy = pygame.Color('#800020')
+
+#Define width and height of game screen
 WIDTH = 1280
 HEIGHT = 700
 
+#Run the timer
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 running = True
@@ -28,11 +38,12 @@ enemy_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
 all_tanks_group = pygame.sprite.Group()
 
+#Create a player and an enemy (INIT)
 player1 = Tank(screen, 100, 100, WIDTH, HEIGHT, bullet_group, color='dark')
 player_group.add(player1)
 enemy1 = EnemyTank(player1, screen, WIDTH-50, HEIGHT-50, WIDTH, HEIGHT, bullet_group, color='red')
 
-
+#Enemy tank spawn
 def spawn_tanks(WIDTH, HEIGHT, num_tanks, enemy_group):
     # spawn more tanks if needed, with a limit
     current_tanks = len(enemy_group)
@@ -46,6 +57,73 @@ def spawn_tanks(WIDTH, HEIGHT, num_tanks, enemy_group):
 # add our sprite to the sprite group
 num_tanks = [1]
 spawn_tanks(WIDTH, HEIGHT, num_tanks, enemy_group)
+
+
+# scoreboard
+score = [0]
+score_font = pygame.font.Font('assets/BebasNeue-Regular.ttf',size=50)
+
+#INSTRUCTION_______________________________________________________________________________________
+
+def make_instructions(screen):
+    # black screen
+    screen.fill(jet)
+    title = ['BULLET HELL']
+    instructions = [
+        '',
+        'Use W to move tank forward',
+        'S to move backward',
+        'A to rotate left',
+        'D to rotate right'
+        'Press Spacebar to shoot a bullet',
+        'Press P to take a screenshot',
+        '',
+        '**PRESS ANY KEY TO START**'
+    ]
+
+    # make an instruction font
+    i_font = pygame.font.Font('assets/BebasNeue-Regular.ttf',size=40)
+    spacing = 80
+    # render (make surface) for each instruction
+    for ii in range(len(instructions)):
+        # render the font
+        font_surf = i_font.render(instructions[ii], True, rose_quartz)
+        # get a rect
+        font_rect = font_surf.get_rect()
+        font_rect.center = (WIDTH//2, spacing + ii * spacing)
+        # blit it to the screen
+        screen.blit(font_surf, font_rect)
+    t_font = pygame.font.Font('assets/BebasNeue-Regular.ttf',size=80)
+    spacing = 80
+    # render title above instructions
+    for ii in range(len(title)):
+        # grab font
+        font_surf = t_font.render(title[ii], True, burgundy)
+        # find rect
+        font_rect = font_surf.get_rect()
+        font_rect.center = (WIDTH//2, spacing + ii * spacing)
+        # blit it
+        screen.blit(font_surf, font_rect)
+
+waiting = 1
+# if we see the spacebar, exit the loop (break)
+while waiting:
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            waiting = 0
+        if event.type == pygame.KEYDOWN:
+            # if any key pressed, break
+            waiting = 0
+
+    make_instructions(screen)
+
+    pygame.display.flip()
+
+
+
+#GAME LOOP_____________________________________________________________________________________________
 
 while running:
     # poll for events
